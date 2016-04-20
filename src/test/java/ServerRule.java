e38e98be66820dc3385a23c9c8dbfbaf48b5bc3b
Mike <mike@epicodus.com>
2016-04-20 16:25:44 -0700

    fix minor spacing issues

--- src/test/java/ServerRule.java
+++ src/test/java/ServerRule.java
@@ -6,7 +6,7 @@ public class ServerRule extends ExternalResource {
   protected void before() {
     String[] args = {};
     App.main(args);
-   }
+  }
 
   protected void after() {
     Spark.stop();
