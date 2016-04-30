fe0e1e3c1780171f023fa0b983002eea1b22f38b
Mike Goren <mgoren@mortalwombat.net>
2016-04-30 01:02:02 -0700

    add db support to spark-template

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,7 +1,21 @@
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class FooTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/____TEST_DATABASE_NAME_HERE_____", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM _____DATABASE_COLUMN_NAME______ *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
 
   @Test
   public void Foo_instantiatesCorrectly() {
@@ -9,6 +23,4 @@ public class FooTest {
     assertEquals(true, testFoo instanceof Foo);
   }
 
-  
-
-}   
+}
